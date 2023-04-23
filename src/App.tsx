4ce9e96d85e5b71630db8145c33141265d7de04c
Patryk Przybylski <darknoriss@gmail.com>
2023-04-23 20:56:34 +0200

    Add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Footer } from './components/Footer';
+import { Game } from './components/Game';
+import { Header } from './components/Header';
+
 export const App = () => {
   return (
-    <div className="w-screen h-screen flex bg-red-400 overflow-hidden">
-      <div></div>
+    <div className="w-screen h-screen flex flex-col bg-gray-800 overflow-hidden">
+      <Header />
+      <Game />
+      <Footer />
     </div>
   );
 };
