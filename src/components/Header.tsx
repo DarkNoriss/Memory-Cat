bec5e2cb54aaa8b2a6ac3d97980fa879ebfdfecb
Patryk Przybylski <darknoriss@gmail.com>
2023-04-23 20:57:54 +0200

    add header with title

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="h-32 w-full flex  items-center">
+      <h1 className="mx-8 text-6xl font-bold">Memory Cat</h1>
+    </header>
+  );
+};
