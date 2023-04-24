7d9e2c9f8984a178eb03e538908932beaa13c80a
Patryk Przybylski <darknoriss@gmail.com>
2023-04-24 17:35:31 +0200

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Footer } from './components/Footer';
 import { Game } from './components/Game';
 import { Header } from './components/Header';
+import { MemoryCatProvider } from './context/memoryCatContext';
 
 export const App = () => {
   return (
     <div className="w-screen h-screen flex flex-col bg-gray-800 overflow-hidden">
-      <Header />
-      <Game />
-      <Footer />
+      <MemoryCatProvider>
+        <Header />
+        <Game />
+        <Footer />
+      </MemoryCatProvider>
     </div>
   );
 };
