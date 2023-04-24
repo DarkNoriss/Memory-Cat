8d1a955337a8aaff2c92eef193b1f3289f104142
Patryk Przybylski <darknoriss@gmail.com>
2023-04-24 17:36:08 +0200

    feature context in menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,26 +1,50 @@
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
+import { useMemoryCatContext } from '../context/memoryCatContext';
 
 export const Menu = () => {
-  const [cards, setCards] = useState<number>(5);
+  const { stateMemoryCat, dispatchMemoryCat } = useMemoryCatContext();
+
+  const handleChange = (value: number) => {
+    dispatchMemoryCat({
+      type: 'setCards',
+      payload: value,
+    });
+  };
+
+  const handleClick = () => {
+    dispatchMemoryCat({
+      type: 'setState',
+      payload: 'play',
+    });
+  };
 
   return (
-    <div className="mb-24 flex-auto flex flex-col justify-center center items-center gap-8">
-      <h2 className="text-4xl">How many cards u want to play with?</h2>
+    <div className="mb-24 flex-auto flex flex-col justify-center items-center gap-8">
+      <h2 className="text-4xl">How many cards do you want to play with?</h2>
       <Box width={400}>
         <Slider
-          defaultValue={5}
           aria-label="Default"
           valueLabelDisplay="auto"
-          onChange={(e, value) => setCards(value as number)}
+          value={stateMemoryCat.cards}
+          onChange={(e, value) => handleChange(value as number)}
           min={2}
           max={20}
         />
       </Box>
-      <Button variant="outlined" onClick={() => console.log(cards)}>
-        Start the game!
+      <Button
+        style={{
+          width: 250,
+          fontWeight: 600,
+          fontSize: 16,
+          padding: '10px 20px',
+          boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+        }}
+        variant="outlined"
+        onClick={handleClick}
+      >
+        Start the game
       </Button>
     </div>
   );
