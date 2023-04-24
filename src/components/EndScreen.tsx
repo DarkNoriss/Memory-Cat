3d0c2bbe607b59e5c50124dee6a54d616999865d
Patryk Przybylski <darknoriss@gmail.com>
2023-04-24 17:35:45 +0200

    add end screen

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -1,3 +1,64 @@
+import Button from '@mui/material/Button';
+import { useMemoryCatContext } from '../context/memoryCatContext';
+
 export const EndScreen = () => {
-  return <h2>EndScreen</h2>;
+  const { stateMemoryCat, dispatchMemoryCat } = useMemoryCatContext();
+
+  const handleClickAgain = () => {
+    dispatchMemoryCat({
+      type: 'setState',
+      payload: 'play',
+    });
+  };
+
+  const handleClickSettings = () => {
+    dispatchMemoryCat({
+      type: 'setState',
+      payload: 'menu',
+    });
+  };
+
+  return (
+    <div className="mb-24 flex-auto flex flex-col justify-center items-center gap-8">
+      <h2 className="text-4xl">Game ended!</h2>
+      {stateMemoryCat.bestTime !== 'none' && (
+        <div className="flex flex-col justify-center items-center text-xl">
+          <p>Best time</p>
+          <p>{stateMemoryCat.bestTime}</p>
+        </div>
+      )}
+      <div className="flex flex-col justify-center items-center text-xl">
+        <p>Current time</p>
+        <p>{stateMemoryCat.time}</p>
+      </div>
+      <div className="flex gap-8">
+        <Button
+          style={{
+            width: 250,
+            fontWeight: 600,
+            fontSize: 16,
+            padding: '10px 20px',
+            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+          }}
+          variant="outlined"
+          onClick={handleClickAgain}
+        >
+          Play again
+        </Button>
+        <Button
+          style={{
+            width: 250,
+            fontWeight: 600,
+            fontSize: 16,
+            padding: '10px 20px',
+            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+          }}
+          variant="outlined"
+          onClick={handleClickSettings}
+        >
+          Change Settings
+        </Button>
+      </div>
+    </div>
+  );
 };
