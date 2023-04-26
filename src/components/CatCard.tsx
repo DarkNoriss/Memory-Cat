ec77bff8821db9055c7c6ce3ed31ee4d1f1f1566
Patryk Przybylski <darknoriss@gmail.com>
2023-04-27 00:51:31 +0200

    add guessed and pass index prop

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -7,14 +7,17 @@ import { CSSTransition } from 'react-transition-group';
 import '../styles/card.css';
 
 type CatCardProps = {
+  index: number;
   card: CardsArrayType;
 };
 
-export const CatCard: React.FC<CatCardProps> = ({ card: { img } }) => {
+export const CatCard: React.FC<CatCardProps> = ({ index, card }) => {
+  const { imgId, guessed } = card;
   const [isFlipped, setIsFlipped] = useState<boolean>(true);
+  const nodeRef = useRef(null);
 
   const handleClick = () => {
-    setIsFlipped(!isFlipped);
+    if (true) setIsFlipped(!isFlipped);
   };
 
   return (
@@ -23,9 +26,9 @@ export const CatCard: React.FC<CatCardProps> = ({ card: { img } }) => {
       style={{ backgroundColor: 'transparent' }}
       onClick={handleClick}
     >
-      <CSSTransition in={isFlipped} timeout={600} classNames="flip">
-        <div className="card">
-          <CardMedia className="card-back" component="img" image={`assets/cards/${img}.png`} />
+      <CSSTransition nodeRef={nodeRef} in={isFlipped} timeout={600} classNames="flip">
+        <div ref={nodeRef} className={`card ${guessed ? '' : 'cursor-pointer'} `}>
+          <CardMedia className="card-back" component="img" image={`assets/cards/${imgId}.png`} />
           <CardContent className="card-front" />
         </div>
       </CSSTransition>
