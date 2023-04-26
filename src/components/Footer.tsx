6d2a92803aac6f9802bdd952ed567dd563c0ff8c
Patryk Przybylski <darknoriss@gmail.com>
2023-04-26 19:52:57 +0200

    fix typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         target="_blank"
         className="flex flex-row items-center gap-1 cursor-pointer hover:brightness-75"
       >
-        <img src="/assets/github.svg" alt="github_icon" className="h-6 aspect-auto" />
+        <img src="assets/github.svg" alt="github_icon" className="h-6 aspect-auto" />
         <h3>DarkNoriss</h3>
       </a>
     </footer>
