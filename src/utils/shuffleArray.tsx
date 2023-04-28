86736daa1596f5080f37416beff1c1649220ecc1
Patryk Przybylski <darknoriss@gmail.com>
2023-04-28 07:44:11 +0200

    move shuffle method to file

---
+++ src/utils/shuffleArray.tsx
@@ -0,0 +1,8 @@
+import { CardsArrayType } from '../context/memoryCatContext';
+
+export const shuffleArray = (array: CardsArrayType[]) => {
+  return array
+    .map((value) => ({ value, sortKey: Math.random() }))
+    .sort((a, b) => a.sortKey - b.sortKey)
+    .map(({ value }) => value);
+};
