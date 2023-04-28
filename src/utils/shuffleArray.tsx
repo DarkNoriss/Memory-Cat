025ee88a49a531b70eddeb3bc1671d026beb71bf
Patryk Przybylski <darknoriss@gmail.com>
2023-04-28 10:04:47 +0200

    improve shuffle algorithm

--- src/utils/shuffleArray.tsx
+++ src/utils/shuffleArray.tsx
@@ -1,8 +1,11 @@
+import { produce } from 'immer';
 import { CardsArrayType } from '../types/CatTypes';
 
-export const shuffleArray = (array: CardsArrayType[]) => {
-  return array
-    .map((value) => ({ value, sortKey: Math.random() }))
-    .sort((a, b) => a.sortKey - b.sortKey)
-    .map(({ value }) => value);
+export const shuffleArray = (array: CardsArrayType[]): CardsArrayType[] => {
+  return produce(array, (draftArray: CardsArrayType[]) => {
+    for (let i = draftArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [draftArray[i], draftArray[j]] = [draftArray[j], draftArray[i]];
+    }
+  });
 };
