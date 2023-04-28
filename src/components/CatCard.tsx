87933a78849333c452f1e68feca03310932770a3
Patryk Przybylski <darknoriss@gmail.com>
2023-04-28 07:44:36 +0200

    add limit to click once every 600ms

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -1,10 +1,9 @@
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import { CardsArrayType } from './CardGrid';
-import { useRef, useState } from 'react';
-import CardContent from '@mui/material/CardContent';
+import { useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import '../styles/card.css';
+import { CardsArrayType, useMemoryCatContext } from '../context/memoryCatContext';
 
 type CatCardProps = {
   index: number;
@@ -12,24 +11,53 @@ type CatCardProps = {
 };
 
 export const CatCard: React.FC<CatCardProps> = ({ index, card }) => {
-  const { imgId, guessed } = card;
-  const [isFlipped, setIsFlipped] = useState<boolean>(true);
+  const { dispatchMemoryCat } = useMemoryCatContext();
+  const { id, imgId, guessed, flipped } = card;
   const nodeRef = useRef(null);
 
-  const handleClick = () => {
-    if (true) setIsFlipped(!isFlipped);
+  function throttle(func: any, limit: number) {
+    let lastFunc: ReturnType<typeof setTimeout> | null;
+    let lastRan: number | null;
+
+    return (...args: any[]): void => {
+      if (!lastRan) {
+        func(...args);
+        lastRan = Date.now();
+      } else {
+        if (lastFunc) clearTimeout(lastFunc);
+        lastFunc = setTimeout(() => {
+          if (Date.now() - (lastRan as number) >= limit) {
+            func(...args);
+            lastRan = Date.now();
+          }
+        }, limit - (Date.now() - (lastRan as number)));
+      }
+    };
+  }
+
+  const flipCard = () => {
+    if (!guessed) {
+      if (!flipped) {
+        dispatchMemoryCat({
+          type: 'FLIP_CARD',
+          payload: { index, id },
+        });
+      }
+    }
   };
+
+  const throttledFlipCard = throttle(flipCard, 500);
 
   return (
     <Card
-      className="flippable-card-container"
+      className={`flippable-card-container ${guessed ? '' : 'cursor-pointer'}`}
       style={{ backgroundColor: 'transparent' }}
-      onClick={handleClick}
+      onClick={throttledFlipCard}
     >
-      <CSSTransition nodeRef={nodeRef} in={isFlipped} timeout={600} classNames="flip">
-        <div ref={nodeRef} className={`card ${guessed ? '' : 'cursor-pointer'} `}>
-          <CardMedia className="card-back" component="img" image={`assets/cards/${imgId}.png`} />
-          <CardContent className="card-front" />
+      <CSSTransition nodeRef={nodeRef} in={!flipped} timeout={600} classNames="flip">
+        <div ref={nodeRef} className="card">
+          <CardMedia className="card-back" component="img" image="assets/catBack.png" />
+          <CardMedia className="card-front" component="img" image={`assets/cards/${imgId}.png`} />
         </div>
       </CSSTransition>
     </Card>
