9dfbc51baaec432ef7667d916d7f3568056a41a9
Patryk Przybylski <darknoriss@gmail.com>
2023-04-30 14:08:04 +0200

    add new var

--- src/types/CatTypes.tsx
+++ src/types/CatTypes.tsx
@@ -1,8 +1,9 @@
 export type MemoryCatType = {
   totalCards: number;
   cardsToMatch: number;
-  elapsedTime: string;
-  bestRecordTime: string;
+  currentTime: number;
+  timerOn: boolean;
+  bestTime: number;
   gameStatus: string;
   cardsData: CardType;
 };
