807d485912763e93d9aaff696833f84b8a49b2bf
Patryk Przybylski <darknoriss@gmail.com>
2023-04-30 14:08:25 +0200

    add import constant

--- src/utils/generateRandomNumber.tsx
+++ src/utils/generateRandomNumber.tsx
@@ -1,7 +1,9 @@
+import { NUMBER_OF_CATS } from '../constants/constants';
+
 export const generateRandomNumber = (usedImages: Set<number>): number => {
   let randomNumber;
   do {
-    randomNumber = Math.floor(Math.random() * 17) + 1;
+    randomNumber = Math.floor(Math.random() * NUMBER_OF_CATS) + 1;
   } while (usedImages.has(randomNumber));
   usedImages.add(randomNumber);
   return randomNumber;
