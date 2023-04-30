34d490fd1ff10a18bb052c55eee38cfb20afe644
Patryk Przybylski <darknoriss@gmail.com>
2023-04-30 14:07:13 +0200

    change cooldown to 300ms

--- src/components/GamePlay.tsx
+++ src/components/GamePlay.tsx
@@ -7,24 +7,29 @@ export const GamePlay = () => {
   const { cardsData } = stateMemoryCat;
 
   useEffect(() => {
-    setTimeout(() => {
-      if (cardsData.cardList.length > 0) {
-        const { selectedCards } = cardsData;
-        if (selectedCards.length === 2) {
-          const actionType =
-            selectedCards[0] === selectedCards[1] ? 'SET_GUESSED_CARDS' : 'HIDE_FLIPPED_CARDS';
-          dispatchMemoryCat({ type: actionType });
-        }
+    if (cardsData.cardList.length > 0) {
+      const { selectedCards } = cardsData;
 
-        const checkWinningCond = cardsData.cardList.every((card) => card.guessed === true);
-        if (checkWinningCond) {
-          dispatchMemoryCat({
-            type: 'UPDATE_GAME_STATE',
-            payload: 'GAME_END',
-          });
-        }
+      if (selectedCards.length === 2) {
+        const actionType =
+          selectedCards[0] === selectedCards[1] ? 'SET_GUESSED_CARDS' : 'HIDE_FLIPPED_CARDS';
+        setTimeout(() => {
+          dispatchMemoryCat({ type: actionType });
+        }, 300);
       }
-    }, 600);
+
+      const checkWinningCond = cardsData.cardList.every((card) => card.guessed === true);
+      if (checkWinningCond) {
+        dispatchMemoryCat({
+          type: 'UPDATE_GAME_STATE',
+          payload: 'GAME_END',
+        });
+
+        // dispatchMemoryCat({
+        //   type: 'CHECK_BEST_TIME',
+        // });
+      }
+    }
   }, [cardsData.cardList]);
 
   return (
